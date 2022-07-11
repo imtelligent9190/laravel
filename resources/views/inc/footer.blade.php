e1e0bbc31adaa7d893a27499a4d1725a440b388b
imtelligent9190 <91670009+imtelligent9190@users.noreply.github.com>
2022-07-11 17:25:36 +0200

    Laravel version from fotter to index

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
     <div>
-        <p>Current Laravel version is {{ app()->version() }}   &copy; 2020 Copyright: Golatowski Sebastian</p>
+        <p>&copy; 2020 Copyright: Golatowski Sebastian</p>
     </div>
 </footer>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="jumbotron text-center">
         <h1>{{$key}}</h1>
-        <p>I hope it works</p>
+        <p>Current Laravel version is {{ app()->version() }}   </p>
         <p><a class="btn btn-dark btn-dg" href="/login" role="button"> Login </a> <a class="btn btn-secondary btn-dg" href="/register" role="button"> Register </a></p>
     </div>
 @endsection
