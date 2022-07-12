588608584a10e82e1f56e072b00e81477b30daae
imtelligent9190 <91670009+imtelligent9190@users.noreply.github.com>
2022-07-12 20:21:09 +0200

    changes in : navbar(changed entirely), posts/index(div is link too)

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,22 +1,65 @@
-<nav id="navbar" class="navbar navbar-expand-sm  ">
+{{-- <nav id="navbar" class="navbar navbar-expand-sm  ">
   <a class="navbar-brand" href="/">Laravel app</a>
+  <div>
+    
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/posts">Blog</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/services">Services</a>
+      </li>
+    </ul>
+  </div>
+  <div class ="nav-left">
   <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="/">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="/posts">Blog</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="/about">About</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="/services">Services</a>
-    </li>
     <li>
       <a class="nav-link" href="/posts/create"> Create_Post </a>
     </li>
   </ul>
+  </div>
   
-</nav>
+</nav> --}}
 
+
+
+<nav id="navbar" class="navbar navbar-expand-sm">
+  
+  <a class="navbar-brand" href="/">
+    <img src="{{asset('logo/logo.jpg')}}" width="60" height="60" alt="logo">
+  </a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/posts">Blog </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/about">About </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/services">Services</a>
+      </li>
+    </ul>
+    
+    <div class="form-inline my-2 my-lg-0">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">  
+      <li class="nav-item active">
+        <a class="nav-link" href="/posts/create">Create_Post</a>
+      </li>
+    </ul>
+    <button type="button" class="btn btn-outline-light">Login</button>
+    <button type="button" class="btn btn-outline-secondary">Register</button>
+    </div>
+    
+  </div>
+</nav>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,8 +4,8 @@
     <h1>Posts</h1>
     @if (count($posts)>0)
         @foreach ($posts as $item)
-            <div class="card p-3 mb-1">
-                <h3><a href="/posts/{{$item->id}}">{{$item->title}}</a></h3>
+            <div class="card p-3 mb-1" onclick="location.href='/posts/{{$item->id}}'">
+                <h3><a href='/posts/{{$item->id}}'>{{$item->title}}</a></h3>
                 <small>Written on {{$item->created_at}}</small>
             </div>
         @endforeach
